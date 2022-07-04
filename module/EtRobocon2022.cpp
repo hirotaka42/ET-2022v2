cbea608715262fe5c830be8c1d600d0419541037
hirotaka42 <kodama.hirotaka.190380@roivy.net>
2022-07-04 12:21:26 +0900

    feat: 開始前に明るさを把握するためLCDに表示する処理を追加

--- module/EtRobocon2022.cpp
+++ module/EtRobocon2022.cpp
@@ -27,14 +27,33 @@ void EtRobocon2022::start()
   /* インスタンス作成 */
   Controller controller;
   Measurer measurer;
+
+  /* 明るさの値(0~100)をLCD表示用にchar MAX100文字分の配列を確保 */
+  char brightnessResult[100]; // 0~100
+  controller.initLcd("Getting Brightness...");
+  controller.setLcdView("Press Touch Sensor to exit.",1);
+
+  // /* タッチセンサが押されるまで待機 */
+  while (!measurer.isPressed()) {
+    sprintf(brightnessResult, "%d",measurer.getBrightness());
+    /* ブリリアンスデータのLCD表示 */ 
+    controller.setLcdView(brightnessResult,2);
+    controller.wait(1000*10*25);
+    /* 0.25秒 wait */
+    /* 早すぎると処理が追いつかない為 */
+  }
+  /* LCD表示領域の初期化 */
+  for(int n=0;n<7;n++) controller.setLcdView("",n);
+  
+
   /* 初期化完了通知 LED */
   controller.setLedColorOrange();
-  controller.setLcdView("Go to the start, ready?",2);
-  controller.wait(1000*1000*3);
-  /* 3秒 wait */
+  controller.initLcd("Go to Ready?");
+  controller.wait(1000*100*15);
+  /* 1.5秒 wait */
 
-  if (_SIM)   controller.setLcdView("Hit SPACE bar to start",2);
-  else        controller.setLcdView("Tap Touch Sensor to start",2);
+  if (_SIM)   controller.setLcdView("Hit SPACE bar to start",1);
+  else        controller.setLcdView("Tap to Start",1);
 
   /* タッチセンサが押されるまで待機 */
   while (!measurer.isPressed()) {
@@ -42,6 +61,9 @@ void EtRobocon2022::start()
       /* 0.08秒 wait */
       /* 早すぎると処理が追いつかない為 */
   }
+  /* LCD表示領域の初期化 */
+  for(int n=0;n<7;n++) controller.setLcdView("",n);
+
   /* タッチセンサが押されたら処理を開始 */
   TraceArea::runTraceArea();
 
